5f7ac88566adca97ad56c6b5f314ad37a8d6d204
Himanshu Mishra <himanshu404mishra@gmail.com>
2025-03-09 21:34:18 +0545

    spiderman

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import app from "./app.js";
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 }); // This is the same as app.listen(3000, () => console.log("Server is running on port 3000"))
